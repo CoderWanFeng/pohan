12fe254e42633e647af32cdba211b9520847bdb5
CoderWanFeng <875143930@qq.com>
2023-04-09 19:31:52 +0800

    split common.py

--- pohan/api/common.py
+++ pohan/api/common.py
@@ -3,25 +3,17 @@
 @Author  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
 @WeChat     ：CoderWanFeng
 @Blog      ：www.python-office.com
-@Date    ：2023/4/6 0:46
+@Date    ：2023/4/9 19:26 
 @Description     ：
 '''
-
-from .common import *
-
-"""
-导包
-"""
+import office
 
 """
-默认关键词
+功能关键词
 """
 
-显示 = print
+加密PDF = office.pdf.encrypt4pdf
 
-"""
-自定义关键词
-"""
-SPLIT_LINE = '=' * 30
-分割线 = ('=' * 30)
+下载图片 = office.image.down4img
 
+图片加水印 = office.image.add_watermark
